469762324c808d6b2abb1287a04e12da93387d92
Giorgio <shadigiorgioislam@gmail.com>
2021-03-10 17:37:35 +0100

    Add news img

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -1,3 +1,9 @@
 @extends('layouts.app')
 
 @section('title', 'News')
+
+@section('content')
+  <div class="foto-uno">
+    <img src="{{ asset('img/fondo-pag-speciali.jpg')}}" alt="img">
+  </div>
+@endsection
